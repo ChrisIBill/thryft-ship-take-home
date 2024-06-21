693a37c1a40622e1fa094f73221565ec35d9b886
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-06-20 20:48:28 -0500

    responsive fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import CheckoutTable from "@/components/table";
 
 export default function Home() {
-  return <CheckoutTable />;
+  return (
+    <div className="flex flex-col gap-5 w-full sm:w-2/3 md:w-1/2">
+      <CheckoutTable />
+    </div>
+  );
 }
